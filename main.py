7ea0cc54ec27bd540dd35c8cb97aff930cb6ce0b
infernumx <spaghetti90314@gmail.com>
2021-04-20 15:47:56 -0700

    Remove threading

--- main.py
+++ main.py
@@ -25,33 +25,21 @@ def push_msg(title, body):
                       }))
 
 
-def worker():
-    cache = set()
-    while True:
-        with open(client_txt, encoding='utf-8') as f:
-            for line in f.readlines()[-500:]:
-                date = re.search(r'(\d+/\d+/\d+ \d+:\d+:\d+)', line).group(0)
-                whisper = re.search(r'@From (.*)', line)
-                if (whisper and (whisper := whisper.group(0)) and
-                   whisper.find('buy your') != -1):
-                    timestamp = datetime.strptime(date,
-                                                  '%Y/%m/%d %H:%M:%S')
-                    t_range = (datetime.now() - relativedelta(seconds=30))
-                    data = whisper + date
-                    if timestamp > t_range and data not in cache:
-                        print(whisper, '\n', date)
-                        push_msg('PoE Whisper', f'{whisper}\n  {date}')
-                        cache.add(data)
+cache = set()
 
-        time.sleep(1)
-
-
-try:
-    os.system('title PoE Whisper Notifier')
-    t = threading.Thread(target=worker)
-    t.daemon = True
-    t.start()
-    while True:
-        time.sleep(100)
-except (KeyboardInterrupt, SystemExit):
-    print('Exiting.')
+while True:
+    with open(client_txt, encoding='utf-8') as f:
+        for line in f.readlines()[-500:]:
+            date = re.search(r'(\d+/\d+/\d+ \d+:\d+:\d+)', line).group(0)
+            whisper = re.search(r'@From (.*)', line)
+            if (whisper and (whisper := whisper.group(0)) and
+               whisper.find('buy your') != -1):
+                timestamp = datetime.strptime(date,
+                                              '%Y/%m/%d %H:%M:%S')
+                t_range = (datetime.now() - relativedelta(seconds=30))
+                data = whisper + date
+                if timestamp > t_range and data not in cache:
+                    print(whisper, '\n', date)
+                    push_msg('PoE Whisper', f'{whisper}\n  {date}')
+                    cache.add(data)
+    time.sleep(1)
