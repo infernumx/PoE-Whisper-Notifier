ff7c038971b43110551d74d186d44aff5da7bb13
infernumx <spaghetti90314@gmail.com>
2021-04-20 15:51:30 -0700

    Notify only on new whispers since script run

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def push_msg(title, body):
 
 
 cache = set()
+start_time = datetime.now()
 
 while True:
     with open(client_txt, encoding='utf-8') as f:
@@ -36,9 +37,8 @@ while True:
                whisper.find('buy your') != -1):
                 timestamp = datetime.strptime(date,
                                               '%Y/%m/%d %H:%M:%S')
-                t_range = (datetime.now() - relativedelta(seconds=30))
                 data = whisper + date
-                if timestamp > t_range and data not in cache:
+                if timestamp >= start_time and data not in cache:
                     print(whisper, '\n', date)
                     push_msg('PoE Whisper', f'{whisper}\n  {date}')
                     cache.add(data)
